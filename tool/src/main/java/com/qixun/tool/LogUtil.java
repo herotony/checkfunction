c463bb7df11f6d7e43d08b8eb173e37d5e9401ee
herotony <herotony@gmail.com>
2017-07-21 17:17:43 +0800

    加入日志功能注释说明

--- tool/src/main/java/com/qixun/tool/LogUtil.java
+++ tool/src/main/java/com/qixun/tool/LogUtil.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
  */
 public class LogUtil {
 
+    //仅仅需要在pom文件中引入logback-classic的jar包会自动引入slf4j-api和logback-core的jar包，然后配置logback.xml即可
+    //然后就可以像本例这样非常开心的开始记录日志了，性能完全不需要担心就是了。
     private static final Logger logger = LoggerFactory.getLogger(LogUtil.class);
 
     public static void INFO(String message){
